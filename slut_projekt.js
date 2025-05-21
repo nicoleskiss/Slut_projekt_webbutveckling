18823b60db2c4f7ac0cab9038e99e5cf91f361f6
Nicole <nicoledvali97@gmail.com>
2025-05-21 12:41:29 +0200

    finslipat med kommentarer

--- slut_projekt.js
+++ slut_projekt.js
@@ -74,3 +74,19 @@ galleryImages.forEach(img => {
         modalImg.src = img.src;
     });
 });
+
+// Smooth scroll när man klickar på en menylänk
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        const targetId = this.getAttribute('href');
+        const targetElement = document.querySelector(targetId);
+
+        if (targetElement) {
+            e.preventDefault();
+            targetElement.scrollIntoView({
+                behavior: 'smooth',
+                block: 'start'
+            });
+        }
+    });
+});
